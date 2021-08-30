d402936d88598034a238047b05ba195004116123
uriri <uribo6165@gmail.com>
2021-08-31 00:06:39 +0900

    feat: チャンネルIDからプレイリストを取得するメソッド実装
    一旦は再生回数上位5つを取得する実装にした
    最終的には全件取得できるようにするが、そうした際のテストコードが難しい。。

--- modules/youtube_api.py
+++ modules/youtube_api.py
@@ -1,3 +1,5 @@
+from typing import Iterator, Tuple
+
 from apiclient.discovery import build
 
 
@@ -25,3 +27,30 @@ class YoutubeAPIUtil:
                 if title == channel_name:
                     id_ = item["snippet"]["channelId"]
                     return id_
+
+    def get_playlists_on_channel(self, channel_id: str) -> Iterator[Tuple[str, str]]:
+        """チャンネルに存在するプレイリスト（再生回数上位5つ）を取得
+
+        Args:
+            channel_id (str): チャンネルID
+
+        Yields:
+            Iterator[Tuple[str, str]]: (プレイリストタイトル, プレイリストID)
+        """
+
+        with build("youtube", "v3", developerKey=self.youtube_api_key) as youbute:
+            search_response = (
+                youbute.search()
+                .list(
+                    part="snippet",
+                    channelId=channel_id,
+                    type="playlist",
+                    order="videoCount",
+                )
+                .execute()
+            )
+
+            for item in search_response["items"]:
+                title = item["snippet"]["title"]
+                id_ = item["id"]["playlistId"]
+                yield (title, id_)
