13fc7dead85a6893b6c47ca448f323a64b8625e8
uriri <uribo6165@gmail.com>
2021-09-15 23:05:39 +0900

    test: Mockの導入
    unittest.mockを導入
    直接APIを叩かなくてもいいようにしていく

--- tests/test_mock_youtube_api.py
+++ tests/test_mock_youtube_api.py
@@ -1,7 +1,7 @@
 import unittest
-from unittest.mock import MagicMock, Mock, patch
+from unittest.mock import patch
 
-from modules.youtube_api import Playlist, YoutubeAPIUtil
+from modules.youtube_api import YoutubeAPIUtil
 
 
 def _get_resource_from_mock(cls, resource):
@@ -9,40 +9,35 @@ def _get_resource_from_mock(cls, resource):
         "items": [
             {
                 "snippet": {
-                    "title": "Subaru Ch. 大空スバル",
-                    "channelId": "UCvzGlP9oQwU--Y0r9id_jnA",
+                    "title": "Mock Channel",
+                    "channelId": "abcdefg1234567",
                 },
             }
         ]
     }
 
 
+@patch.object(YoutubeAPIUtil, "_get_resource_from_api", new=_get_resource_from_mock)
 class TestYoutubeAPI(unittest.TestCase):
     def test_func(self):
-        with patch.object(
-            YoutubeAPIUtil, "_get_resource_from_api", new=_get_resource_from_mock
-        ):
-            youtube_api_util = YoutubeAPIUtil(api_key="")
-            actual = youtube_api_util._get_resource_from_api(resource={})
-            expected = {
-                "items": [
-                    {
-                        "snippet": {
-                            "title": "Subaru Ch. 大空スバル",
-                            "channelId": "UCvzGlP9oQwU--Y0r9id_jnA",
-                        },
-                    }
-                ]
-            }
-            self.assertEqual(actual, expected)
+        youtube_api_util = YoutubeAPIUtil(api_key="")
+        actual = youtube_api_util._get_resource_from_api(resource={})
+        expected = {
+            "items": [
+                {
+                    "snippet": {
+                        "title": "Mock Channel",
+                        "channelId": "abcdefg1234567",
+                    },
+                }
+            ]
+        }
+        self.assertEqual(actual, expected)
 
     def test_get_channel_id(self):
         """チャンネルIDを取得するテスト"""
-        with patch.object(
-            YoutubeAPIUtil, "_get_resource_from_api", new=_get_resource_from_mock
-        ):
-            channel_name = "Subaru Ch. 大空スバル"
-            youtube_api_util = YoutubeAPIUtil(api_key="")
-            actual = youtube_api_util.get_channel_id_from_channel_name(channel_name)
-            expected = "UCvzGlP9oQwU--Y0r9id_jnA"
-            self.assertEqual(actual, expected)
+        channel_name = "Mock Channel"
+        youtube_api_util = YoutubeAPIUtil(api_key="")
+        actual = youtube_api_util.get_channel_id_from_channel_name(channel_name)
+        expected = "abcdefg1234567"
+        self.assertEqual(actual, expected)
